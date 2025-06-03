565407fa81dad32d6b79037d491ae6fdc7ccdec6
SaltySalt77 <hyna0707@gmail.com>
2025-06-03 15:48:12 +0900

    Feat: Add text styles for design system

---
+++ src/styles/textStyles.ts
@@ -0,0 +1,14 @@
+const textStyles = {
+  title1: "font-bold text-[2.5rem] leading-[3.75rem] font-pretendard",
+  title2: "font-bold text-[1.75rem] font-pretendard",
+  title3: "font-bold text-[1.5rem] font-pretendard",
+  title3_1: "font-medium text-[1.5rem] font-pretendard",
+  title4: "font-medium text-[2rem] font-pretendard",
+  text1: "font-medium text-[1.25rem] font-pretendard",
+  text2: "font-normal text-[1rem] leading-[1.5625rem] font-pretendard",
+  text2_1: "font-bold text-[1rem] font-pretendard",
+  sub1: "font-normal text-[1.5rem] font-pretendard",
+  sub2: "font-medium text-[0.875rem] font-pretendard",
+};
+
+export default textStyles;
