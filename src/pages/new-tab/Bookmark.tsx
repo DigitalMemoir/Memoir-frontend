0d11fc60823998b336e50870c025f08910adae2a
SaltySalt77 <hyna0707@gmail.com>
2025-06-05 23:45:07 +0900

    Feat: Add bookmark component

---
+++ src/pages/new-tab/Bookmark.tsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+
+const Bookmark = ({ href }: { href: string }) => {
+  const isExtension = window.location.protocol === 'chrome-extension:';
+  const faviconUrl = isExtension
+    ? `chrome://favicon2/?size=64&pageUrl=${encodeURIComponent(href)}`
+    : `https://www.google.com/s2/favicons?sz=64&domain_url=${encodeURIComponent(href)}`;
+  return (
+    <motion.a
+      href={href}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      className={'inline-block w-fit h-fit p-8 shadow-default-1 rounded-full'}
+    >
+      <img
+        src={faviconUrl}
+        alt={`${href} favicon`}
+        className={'w-16 h-16 rounded-full'}
+        loading={'lazy'}
+        draggable={'false'}
+      />
+    </motion.a>
+  );
+};
+
+export default Bookmark;
