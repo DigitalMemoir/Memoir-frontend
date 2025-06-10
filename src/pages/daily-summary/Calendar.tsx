b916a193945f57e7840cda413d35e148f4cf5d2e
SaltySalt77 <hyna0707@gmail.com>
2025-06-10 22:51:34 +0900

    Feat: Customized event

--- src/pages/daily-summary/Calendar.tsx
+++ src/pages/daily-summary/Calendar.tsx
@@ -2,8 +2,14 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import Day from './Calendar/Day';
 import './Calendar.css';
+import type { IEvent } from '../../types/ICalendar';
+import Event from './Calendar/Event';
 
 const Calendar = () => {
+  const events: Array<IEvent> = [
+    { title: 'Meeting1', start: new Date('2025-6-29'), allDay: true },
+    { title: 'Meeting2', start: new Date('2025-6-30'), allDay: true },
+  ];
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
@@ -16,6 +22,9 @@ const Calendar = () => {
         right: 'next',
       }}
       dayCellContent={Day}
+      events={events}
+      eventDisplay={'block'}
+      eventContent={Event}
     />
   );
 };
---
+++ src/pages/daily-summary/Calendar/Event.tsx
@@ -0,0 +1,40 @@
+import type { EventContentArg } from '@fullcalendar/core/index.js';
+import clsx from 'clsx';
+import textStyles from '../../../styles/textStyles';
+
+const colorPalette = [
+  'bg-[#FFF3BD]',
+  'bg-[#CFFFCF]',
+  'bg-[#FFD6D6]',
+  'bg-[#B9F6FF]',
+  'bg-[#DEC1FF]',
+];
+
+const getColorClass = (key: string): string => {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash = key.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const index = Math.abs(hash) % colorPalette.length;
+  return colorPalette[index];
+};
+
+const Event = (arg: EventContentArg) => {
+  const colorClass = getColorClass(arg.event.title);
+  return (
+    <div
+      className={clsx(
+        colorClass,
+        'w-full h-5 pl-3',
+        'flex flex-row items-center justify-start',
+        // 'font-medium text-[10.4px] font-pretendard',
+        textStyles.sub2,
+        'text-text-body'
+      )}
+    >
+      {arg.event.title}
+    </div>
+  );
+};
+
+export default Event;
