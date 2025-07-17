ff01cef27eaa9f80bb51edf7786f5ac0f322577d
SaltySalt77 <hyna0707@gmail.com>
2025-07-17 16:31:24 +0900

    Feat: Connect daily browser usage API

--- src/pages/usage/UsagePage.tsx
+++ src/pages/usage/UsagePage.tsx
@@ -1,6 +1,11 @@
+import dayjs from 'dayjs';
+import axiosInstance from '../../lib/axiosInstance';
 import type { IActivityStatsResponse } from '../../types/IUsage';
+import { getBrowsingHistory } from '../../utils/getBrowsingHistory';
 import Statistics from './Statistics';
 import TotalTimes from './TotalTimes';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 const data: IActivityStatsResponse = {
   activityStats: {
@@ -86,6 +91,37 @@ const data: IActivityStatsResponse = {
 };
 
 const UsagePage = () => {
+  const [data, setData] = useState<IActivityStatsResponse | undefined>(
+    undefined
+  );
+
+  const getUsageData = async () => {
+    const today = dayjs().format('YYYY-MM-DD');
+    const browsingHistory = await getBrowsingHistory(today);
+    const response = await axiosInstance.post('/api/time', browsingHistory);
+
+    return response.data;
+  };
+
+  useEffect(() => {
+    mutate();
+  }, []);
+
+  const { mutate } = useMutation({
+    mutationFn: getUsageData,
+    onSuccess: (data) => {
+      setData(data.data);
+    },
+    onError: (error) => {
+      console.error('오늘의 분석 기록을 가져오지 못했어요.');
+      console.error(error);
+    },
+  });
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div
       className={
@@ -94,7 +130,7 @@ const UsagePage = () => {
     >
       <Statistics hourlyData={data.activityStats.hourlyActivityBreakdown} />
       <div className={'flex flex-row items-center gap-6 mt-10'}>
-        {data.activityStats.categorySummaries
+        {data?.activityStats.categorySummaries
           .sort((a, b) => (a.totalTimeMinutes > b.totalTimeMinutes ? -1 : 1))
           .slice(0, 3)
           .map((summary) => (
