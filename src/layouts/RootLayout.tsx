05fe50f27acdd6a4c3f1b883eabcb13aebc7c829
SaltySalt77 <hyna0707@gmail.com>
2025-07-23 21:23:14 +0900

    Feat: Connect profile API

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,9 +3,31 @@ import BellIcon from '../assets/icons/Bell.svg';
 import { Outlet } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { useDrawerStore } from '../states/useDrawerStore';
+import axiosInstance from '../lib/axiosInstance';
+import { useQuery } from '@tanstack/react-query';
+import type { IProfile } from '../types/IProfile';
+import { useProfile } from '../states/useProfile';
 
 const RootLayout = () => {
   const { isOpen, openDrawer, closeDrawer } = useDrawerStore();
+
+  const { setProfile } = useProfile();
+
+  const getProfile = async () => {
+    const response = await axiosInstance.get('/api/users/profile');
+
+    console.log('Profile data fetched:', response.data);
+    setProfile(response.data.data);
+
+    return response.data.data;
+  };
+
+  const { data } = useQuery<IProfile>({
+    queryKey: ['profileData'],
+    queryFn: getProfile,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  });
 
   return (
     <motion.div
@@ -21,7 +43,8 @@ const RootLayout = () => {
           className={'w-[2.08vw] h-[4.07vh] px-0.5 text-gray-3'}
         />
         <img
-          src={'https://picsum.photos/80'}
+          referrerPolicy="no-referrer"
+          src={data?.profileUrl}
           className={'w-[4.17vw] h-auto aspect-square rounded-full'}
         />
       </div>
